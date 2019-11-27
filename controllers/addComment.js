6fc009b51e352b803a14ff5ab5b61f3029ecf265
webbiqing <webwbqing@163.com>
2019-11-27 10:31:22 +0800

    评论接口修改

--- controllers/addComment.js
+++ controllers/addComment.js
@@ -4,12 +4,12 @@ const verify = require("../models/verify")
 const setFrontData = require("../utils/setFrontData")
 
 module.exports = async (ctx,next) => {
-    const { blog_id,content,create_time } = ctx.request.body;
+    const { blog_id,content } = ctx.request.body;
     const dataArr = ctx.request.headers.authorization.split(' ');
     const token = dataArr[1];
     let userResult = await verify.getTokenUser(token);
     if(!userResult.length) return ctx.body = setFrontData('fail',{},'未找到该用户！');
     if(!blog_id) return ctx.body = setFrontData('fail',{},'关联id为空，不能进行评论！');
-    let result =  await comment.addComment(blog_id,content,userResult[0].username,create_time);
+    let result =  await comment.addComment(blog_id,content,userResult[0].username);
     ctx.body = setFrontData('success',{result},'成功')
 }
--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ const sqlClient = require("../tools/index")
 const escape = require('../tools/escape')
 
 const comment  = {
-    async addComment(blog_id,content,create_user,create_time){
+    async addComment(blog_id,content,create_user){
         return await sqlClient.query(
             escape`INSERT INTO comment (blog_id,content,create_user,create_time) 
                    VALUES 
