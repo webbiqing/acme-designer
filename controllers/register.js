b04982dfb16da89433d85c2f3a387da0ebda3a14
webbiqing <webwbqing@163.com>
2019-11-27 18:09:39 +0800

    注册校验

--- controllers/register.js
+++ controllers/register.js
@@ -6,7 +6,11 @@ const encryto = require("../utils/encryto")
 module.exports = async (ctx,next) => {
     const {username,pwd} = ctx.request.body;
     if(!(pwd && username)) return ctx.body = setFrontData('fail',{},'用户名或者密码不能为空！');
+    const userResult = await register.findUserByUserName(username);
+    if(userResult.length){
+        return ctx.body = setFrontData('fail',{userResult},'用户名已存在')
+    }
     const password = encryto(pwd);
-    let result =  await register(username,password);
+    let result =  await register.addUser(username,password);
     ctx.body = setFrontData('success',{result},'成功')
 }
--- models/register.js
+++ models/register.js
@@ -1,10 +1,19 @@
 const sqlClient = require("../tools/index")
 const escape = require('../tools/escape')
 
-module.exports = async function(username,pwd,){
-    return await sqlClient.query(
-        escape`INSERT INTO user (username,pwd,create_time) 
+const register = {
+    async addUser(username,pwd){
+        return await sqlClient.query(
+            escape`INSERT INTO user (username,pwd,create_time) 
                    VALUES 
                    (${username},${pwd},NOW())`
-    )
+        )
+    },
+    async findUserByUserName(username){
+        return await sqlClient.query(
+            escape`select count(*) as countnum from user where username = ${username}`
+        )
+    },
 }
+
+module.exports = register;
