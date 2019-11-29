72c1a32b365b948aa85a050c81bae9eb78fc0f66
webbiqing <webwbqing@163.com>
2019-11-29 11:45:51 +0800

    解决bug

--- controllers/register.js
+++ controllers/register.js
@@ -7,7 +7,7 @@ module.exports = async (ctx,next) => {
     const {username,pwd} = ctx.request.body;
     if(!(pwd && username)) return ctx.body = setFrontData('fail',{},'用户名或者密码不能为空！');
     const userResult = await register.findUserByUserName(username);
-    if(userResult.length){
+    if(userResult.length && userResult[0].countnum > 0){
         return ctx.body = setFrontData('fail',{userResult},'用户名已存在')
     }
     const password = encryto(pwd);
